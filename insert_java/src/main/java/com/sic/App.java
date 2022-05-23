ad809f65ec8a21606a6b01a8b0185151e3c42f59
armando-cruz-cruz <armando.cruz@uvmnet.edu>
2022-05-23 09:35:15 -0500

    primer
    primer commit

---
+++ insert_java/src/main/java/com/sic/App.java
@@ -0,0 +1,28 @@
+package com.sic;
+import java.sql.*; 
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        try { 
+            //Llamada a la libreria mysql conector 
+            // Class.forName("com.mysql.jdbc.Driver"); 
+            //Nueva conexión                        conexión a localhost |   nombre de bd | ususario mysql | contraseña   
+             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/datos", "usuariotabla", "pass1234"); 
+            //Nuevo statement para crear datos 
+             Statement stmt = con.createStatement(); 
+            //Creación de Query | Insertar valores 
+            //Estos valores deben coincidir con los tipos de datos que se asignaron durante la creación de la tabla 
+             stmt.executeUpdate("INSERT INTO alumnos VALUES(2,'Santiago','López','20524700')"); 
+            //Se cierra la conexión 
+             con.close(); 
+       } catch (Exception e) { 
+            //Imprimir errores 
+             System.out.println(e); 
+       }
+    }
+}
